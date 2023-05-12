7fcc9b190714c961a09a944b8218b18a59c766ef
chenshao107 <85855440+chenshao107@users.noreply.github.com>
2023-05-12 21:59:01 +0800

    新增管理员处理报修功能

---
+++ windows_programing_curriculum_design/OneMaintain.Designer.cs
@@ -0,0 +1,114 @@
+﻿using System.Runtime.InteropServices;
+
+namespace windows_programing_curriculum_design
+{
+
+    //该控件是RoomMaintain的一个控件，代表一条纪录
+    partial class OneMaintain
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1015, 10);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1015, 43);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(839, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(848, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "✔";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // OneMaintain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "OneMaintain";
+            this.Size = new System.Drawing.Size(1015, 53);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Button button1;
+
+        //表示房号
+        public int _roomId;
+    }
+}
--- windows_programing_curriculum_design/RootMaintainForm.cs
+++ windows_programing_curriculum_design/RootMaintainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,53 @@ namespace windows_programing_curriculum_design
         public RootMaintainForm()
         {
             InitializeComponent();
+            //定义数据库实体
+            Database2Entities2 database2Entities2;
+            try
+            {
+                database2Entities2 = new Database2Entities2();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("数据库连接异常");
+                throw ex;
+            }
+            //获取报修表中所有数据
+            var records = database2Entities2.Repair;
+            foreach (var record in records)
+            {
+                //新建一行记录，显示上去
+                var oneLine = new OneMaintain(record.RoomId, record.Problem+"  预约时间："+record.AppointmentDate);
+                oneLine.Dock = DockStyle.Top;
+                //将一个RemoveOneRecond函数绑定到该按钮；
+                oneLine.button1.Click +=  delegate (object sender1, EventArgs e1) { RemoveOneRecond(sender1 , ref oneLine); };//匿名传参
+                this.panel1.Controls.Add(oneLine);
+            }
+        }
+        public void RemoveOneRecond(object sender,ref OneMaintain oneLine)
+        {
+            //oneMaintain为发出信号的控件
+            //定义数据库实体
+            Database2Entities2 database2Entities2;
+            try
+            {
+                database2Entities2 = new Database2Entities2();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("数据库连接异常");
+                throw ex;
+            }
+            Repair oneRecond=new Repair();
+            oneRecond.RoomId = oneLine._roomId;
+            database2Entities2.Entry<Repair>(oneRecond).State = EntityState.Deleted;
+            int i = database2Entities2.SaveChanges();
+            panel1.Controls.Remove(oneLine);
+        }
+
+        private void RootMaintainForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
