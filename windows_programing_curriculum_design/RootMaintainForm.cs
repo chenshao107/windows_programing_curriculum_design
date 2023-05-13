a4b182ecf595929676604c514721bdfa9d6ca387
chenshao107 <85855440+chenshao107@users.noreply.github.com>
2023-05-13 13:39:56 +0800

    加了一点注释

--- windows_programing_curriculum_design/RootMaintainForm.cs
+++ windows_programing_curriculum_design/RootMaintainForm.cs
@@ -53,10 +53,15 @@ namespace windows_programing_curriculum_design
                 MessageBox.Show("数据库连接异常");
                 throw ex;
             }
+            //新建一条记录
             Repair oneRecond=new Repair();
+            //填入对应的房号
             oneRecond.RoomId = oneLine._roomId;
+            //提交删除语句
             database2Entities2.Entry<Repair>(oneRecond).State = EntityState.Deleted;
+            //保存设置
             int i = database2Entities2.SaveChanges();
+            //从UI界面移除该控件
             panel1.Controls.Remove(oneLine);
         }
 
